da2dd45b33c5de7c3774b5c9f03ae9a00110b526
viraldL <107942715+viraldL@users.noreply.github.com>
2022-11-22 19:51:32 +0100

    Add two modes

--- app.js
+++ app.js
@@ -10,11 +10,35 @@ const gameBoard = (function(){
             displayController.btnWork(field, gameBoard, i, field);
         }
         const main = document.querySelector("main");
-        main.classList.add("boardOn")
+        main.classList.add("boardOn");
+
         main.appendChild(board);
     }
 
     return { render };
+})();
+
+const Player = function(nick, mark) {
+    const showName = () => {}
+    return {nick};
+}
+
+const chooseMode = (function() {
+    const twoPlayer = document.createElement("div");
+    const botGame = document.createElement("div");
+    function render() {
+        twoPlayer.classList.add("twoPlayerBtn");
+        botGame.classList.add("botGameBtn");
+        twoPlayer.innerText = "2 Players";
+        botGame.innerText = "Bot";
+
+        const main = document.querySelector("main");
+        main.classList.add("chooseModeOn");
+        main.appendChild(twoPlayer);
+        main.appendChild(botGame);
+    }
+
+    return { render, twoPlayer, botGame };
 })();
 
 const displayController = (function(){
@@ -28,19 +52,33 @@ const displayController = (function(){
 
     return { btnWork }
 })();
-gameBoard.render();
 
-const Player = function(nick) {
-    return {nick};
-}
+const modeController = (function() {
+    chooseMode.twoPlayer.addEventListener("click" , () => {
+        chooseMode.twoPlayer.classList.add("hide");
+        chooseMode.botGame.classList.add("slideRight");
+    })
+    chooseMode.botGame.addEventListener("click" , () => {
+        chooseMode.twoPlayer.classList.add("slideLeft");
+        chooseMode.botGame.classList.add("hide");
+    })
+})();
+
+const namePlayers = (function() {
+
+})();
+
 
 const animate = (function() {
     window.addEventListener("DOMContentLoaded", () => {
         const header = document.querySelector("header");
         const footer = document.querySelector("footer");
+        chooseMode.render();
         setTimeout(() => {
             header.classList.add("show");
             footer.classList.add("show");
+           chooseMode.botGame.classList.add("show2");
+           chooseMode.twoPlayer.classList.add("show2");
         }, 1000)
     })
 })();
